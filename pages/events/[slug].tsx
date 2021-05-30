07728e70ca0a70b44c6e895028b2ed7b9ea758ae
Alec-BoomSports <alec@playboomsports.com>
2021-05-30 14:20:53 -0400

    Added some basic files

---
+++ pages/events/[slug].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IProps {}
+
+const EventPage: React.FC<IProps> = (props) => {
+  return (
+    <div>
+      <h1>Slug Event</h1>
+    </div>
+  );
+};
+
+export default EventPage;
---
+++ pages/events/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IProps {}
+
+const index: React.FC<IProps> = (props) => {
+  return (
+    <div>
+      <h1>My Events</h1>
+    </div>
+  );
+};
+
+export default index;
